5a4566b0734263b2dfaa76ed481c66aa677dc21a
LuxProtoss <zerox12311@gmail.comm>
2022-06-30 15:04:30 +0800

    Add basic method without restframework

--- app/authorization/views.py
+++ app/authorization/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from app.authorization.serializers import UserSerializer
 from .models import User
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
@@ -10,5 +12,14 @@ class UserViewSet(viewsets.ModelViewSet):
 
     ---
     """
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@csrf_exempt
+def hello_world(request: HttpRequest):
+    if request.method == "POST":
+        print(request.body)
+
+    return JsonResponse({"status": "OK"})
--- setting/urls.py
+++ setting/urls.py
@@ -20,6 +20,7 @@ from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from .rest_router import router
+from app.authorization import views
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -39,4 +40,5 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path('hello', views.hello_world)
 ]
